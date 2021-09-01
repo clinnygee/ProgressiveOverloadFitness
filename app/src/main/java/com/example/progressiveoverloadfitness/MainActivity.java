f5e1cdb926b9146536835b7c597f9722862aaec5
clinnygee <clintongillespie@outlook.com>
2021-09-01 11:53:18 +1000

    added bottom navigation to application

--- app/src/main/java/com/example/progressiveoverloadfitness/MainActivity.java
+++ app/src/main/java/com/example/progressiveoverloadfitness/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.progressiveoverloadfitness;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigatin_view);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_fragment);
+//        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_fragment);
+//        NavController navController = navHostFragment.getNavController();
+//        bottomNavigationView.setUpWithNavController(navController);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
+
 }
